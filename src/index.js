5ee0289af8aac2b1b0586e13b37a9ff41bbfad7e
Clinton Dsouza <3950377+clinton2111@users.noreply.github.com>
2020-10-14 00:00:48 +0400

    Added the ability to update to Firebase

--- src/index.js
+++ src/index.js
@@ -4,11 +4,25 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const firebase = require('firebase');
+require('firebase/firestore')
+
+// Initialize Firebase
+firebase.initializeApp({
+	apiKey: process.env.REACT_APP_Firebase_apiKey,
+	authDomain: process.env.REACT_APP_Firebase_authDomain,
+	databaseURL: process.env.REACT_APP_Firebase_databaseURL,
+	projectId: process.env.REACT_APP_Firebase_projectId,
+	storageBucket: process.env.REACT_APP_Firebase_storageBucket,
+	messagingSenderId: process.env.REACT_APP_Firebase_messagingSenderId,
+	appId: process.env.REACT_APP_Firebase_appId
+});
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<App/>
+	</React.StrictMode>,
+	document.getElementById('evernote-container')
 );
 
 // If you want your app to work offline and load faster, you can change
